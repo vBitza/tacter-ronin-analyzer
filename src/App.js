df726a5142d0481cf915f8d8654e38d73487a57e
Gabriel Onea <gabriel.onea@radcom.com>
2021-11-30 00:48:14 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import logo from './logo.svg';
+import backgroundImage from './assets/axieBackground.jpg'
+import './App.css';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+
+function App() {
+  return (
+    <div
+      className="container"
+      style={{
+        backgroundSize: 'cover',
+        backgroundImage: `url(${backgroundImage})`,
+      }}
+    >
+      <div className="gradient"
+        style={{
+          alignItems: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+          width: '100%',
+          height: '100%',
+          background: "linear-gradient(180deg, rgba(21, 23, 25, 0.25), rgba(0, 0, 0, 1))"
+        }}
+      >
+        <p
+          style={{
+            marginTop: '5%',
+            marginBottom: 0,
+            color: 'white',
+            fontSize: '40px'
+          }}
+        >
+          Tacter Ronin Wallet Analyzer
+        </p>
+        <p
+          style={{
+            marginTop: '20px',
+            color: 'white',
+            fontSize: '20px'
+          }}
+        >
+          Description
+        </p>
+        <Container
+          style={{
+            width: '50%',
+            marginTop: '100px'
+          }}
+        >
+          <Box
+            sx={{
+              borderRadius: '25px',
+              border: '1px solid white',
+              bgcolor: '#212326',
+              height: '300px',
+              width: '100%'
+            }}
+          >
+          </Box>
+        </Container>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
